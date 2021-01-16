224838e52fe7eabac3d4416fed502369cd00591a
Scorpius <el-zaiat@msn.com>
2021-01-16 22:32:55 +0200

    Add method multiple results query

--- core/Model.php
+++ core/Model.php
@@ -5,19 +5,27 @@ abstract class Model
     /** @noinspection SqlResolve */
     public static final function getById(int $id): self
     {
-        $className = get_called_class();
-        $query = PDOWrapper::getConnection()->prepare("SELECT * FROM " . $className . " WHERE id='" . $id . "'");
-        $query->setFetchMode(PDO::FETCH_CLASS, $className);
-        $query->execute();
-        return $query->fetch();
+        $stmt = PDOWrapper::getConnection()->prepare("SELECT * FROM " . get_called_class() . " WHERE id='" . $id . "'");
+        return self::executeQuerySingle($stmt);
     }
 
     public static final function getByAttr(string $attr, string $val): self
     {
-        $className = get_called_class();
-        $query = PDOWrapper::getConnection()->prepare("SELECT * FROM " . $className . " WHERE " . $attr . "=' . $val . '");
-        $query->setFetchMode(PDO::FETCH_CLASS, $className);
-        $query->execute();
-        return $query->fetch();
+        $stmt = PDOWrapper::getConnection()->prepare("SELECT * FROM " . get_called_class() . " WHERE " . $attr . "=' . $val . '");
+        return self::executeQuerySingle($stmt);
+    }
+
+    private static final function executeQuerySingle(PDOStatement $stmt): self
+    {
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    private static final function executeQueryMultiple(PDOStatement $stmt): array
+    {
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 }
