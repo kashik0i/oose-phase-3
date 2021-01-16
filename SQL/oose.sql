470504ce5f8d6cc3e49e9a843e7c225be4611c43
Scorpius <el-zaiat@msn.com>
2021-01-16 20:38:03 +0200

    Rename primary keys to 'id' instead of 'model_id'

--- SQL/oose.sql
+++ SQL/oose.sql
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `activity`
 --
 
-CREATE TABLE `activity` (
-  `activity_id` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT current_timestamp()
+CREATE TABLE `activity`(
+                           `id`         int(11)   NOT NULL,
+                           `created_at` timestamp NULL DEFAULT current_timestamp(),
+                           `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -40,11 +40,12 @@ CREATE TABLE `activity` (
 -- Table structure for table `campaign`
 --
 
-CREATE TABLE `campaign` (
-  `campaign_id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `activity_id` int(11) DEFAULT NULL
+CREATE TABLE `campaign`
+(
+    `id`          int(11) NOT NULL,
+    `title`       varchar(255) DEFAULT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `activity_id` int(11)      DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -53,13 +54,15 @@ CREATE TABLE `campaign` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `birth_date` date DEFAULT NULL,
-  `activity_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `user`
+(
+    `id`          int(11) NOT NULL,
+    `first_name`  varchar(50) DEFAULT NULL,
+    `last_name`   varchar(50) DEFAULT NULL,
+    `birth_date`  date        DEFAULT NULL,
+    `activity_id` int(11)     DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -69,21 +72,21 @@ CREATE TABLE `user` (
 -- Indexes for table `activity`
 --
 ALTER TABLE `activity`
-  ADD PRIMARY KEY (`activity_id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `campaign`
 --
 ALTER TABLE `campaign`
-  ADD PRIMARY KEY (`campaign_id`),
-  ADD KEY `campaign_activity_activity_id_fk` (`activity_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `campaign_activity_activity_id_fk` (`activity_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `user_activity_activity_id_fk` (`activity_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `user_activity_activity_id_fk` (`activity_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -93,19 +96,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `activity`
 --
 ALTER TABLE `activity`
-  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `campaign`
 --
 ALTER TABLE `campaign`
-  MODIFY `campaign_id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -115,13 +118,13 @@ ALTER TABLE `user`
 -- Constraints for table `campaign`
 --
 ALTER TABLE `campaign`
-  ADD CONSTRAINT `campaign_activity_activity_id_fk` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`activity_id`);
+    ADD CONSTRAINT `campaign_activity_activity_id_fk` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`id`);
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_activity_activity_id_fk` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`activity_id`);
+    ADD CONSTRAINT `user_activity_activity_id_fk` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
