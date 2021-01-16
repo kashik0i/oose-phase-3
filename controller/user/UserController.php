7fbef6c1fc641319a95182b81a876523ef9b313e
Scorpius <el-zaiat@msn.com>
2021-01-16 20:57:51 +0200

    Add homepage example

--- controller/user/UserController.php
+++ controller/user/UserController.php
@@ -3,6 +3,17 @@ require_once "../../core/Controller.php";
 
 class UserController extends Controller
 {
+    protected function default()
+    {
+        echo 'Homepage<br>';
+        echo '<a href="./UserController.php?action=getProfile&id=1">Profile id 1</a>';
+    }
+
+    protected function getProfile(array $args)
+    {
+        echo "Profile";
+        print_r($args);
+    }
 }
 
 $controller = new UserController();
